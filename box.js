3ed5a5ea3bc26a9aacd137b79c6db5d69dab2eff
JasmineMarwaha <67989737+JasmineMarwaha@users.noreply.github.com>
2020-08-17 19:42:03 -0700

    Update box.js

--- box.js
+++ box.js
@@ -1,17 +1,35 @@
 class Box  {
-    constructor(x, y, width, height){ 
+    constructor(x, y, width, height, color){ 
       var options = {
-        isStatic: true,
+        restitution: 0.4,
+        friction: 0,
       }
       this.body = Bodies.rectangle(x, y, width, height, options);
       this.width = width;
       this.height = height;
+      this.color = color;
+      this.Visiblity = 255;
       World.add(world, this.body);
     }
     display() {
-      var pos = this.body.position;
-      rectMode(CENTER);
-      fill("pink");
-      rect(pos.x, pos.y, this.width, this.height);
+      if(this.body.speed < 6) {
+        var pos = this.body.position;
+        var angle = this.body.angle;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle)
+        fill(this.color);
+        rectMode(CENTER);
+      rect(0, 0 ,this.width, this.height);
+      pop();
+      }
+      else {
+        World.remove(world, this.body);
+        push();
+        this.Visiblity = this.Visiblity -5;
+        tint(255, this.Visiblity);
+        pop();
+
+      }
     }
   }
